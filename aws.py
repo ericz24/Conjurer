1979f54e366d4f9461b45bebeb656a1139250dea
Bo Zhang <wavezb@Bos-MacBook-Pro.local>
2022-08-28 16:50:54 -0700

    Add aws.py

---
+++ aws.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+
+def getUser(id, name):
+    dynamoDB=boto3.resource('dynamodb',aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    usrTable = dynamoDB.Table('User') 
+    return usrTable.get_item(Key={
+                'id': id,
+                'name': name
+    })
+
+print(getUser('e793419b-17db-4938-a719-db8bcb929225', 'Eric Zhang'))
